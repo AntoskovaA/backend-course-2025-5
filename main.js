08dddcf170c6cd35d62fea390b62d4732020558b
Alina bc2024-5 <ALINA.ANTOSHKOVA@lnu.edu.ua>
2026-04-09 12:27:16 +0300

    part2 code d

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 const http = require('node:http');
 const fs = require('node:fs');
 const { program } = require('commander');
+const path = require('node:path');
 
 program
-  .requiredOption('-h, --host <host>', 'server host')
-  .requiredOption('-p, --port <port>', 'server port')
-  .requiredOption('-c, --cache <cache>', 'cache directory path');
+    .requiredOption('-h, --host <host>', 'server host')
+    .requiredOption('-p, --port <port>', 'server port')
+    .requiredOption('-c, --cache <cache>', 'cache directory path');
 
 program.parse(process.argv);
 
@@ -13,11 +14,45 @@ const { host, port, cache } = program.opts();
 
 fs.mkdirSync(cache, { recursive: true });
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.end('Server is alive!!');
+const server = http.createServer(async (req, res) => {
+    const code = req.url.slice(1);
+    const filePath = path.join(cache, `${code}.jpg`);
+
+    if (req.method === 'GET') {
+        try {
+            const file = await fs.promises.readFile(filePath);
+            res.writeHead(200, { 'Content-Type': 'image/jpeg' });
+            res.end(file);
+        } catch {
+            res.statusCode = 404;
+            res.end('Not found');
+        }
+
+    } else if (req.method === 'PUT') {
+        const chunks = [];
+        req.on('data', chunk => chunks.push(chunk));
+        req.on('end', async () => {
+            await fs.promises.writeFile(filePath, Buffer.concat(chunks));
+            res.statusCode = 201;
+            res.end('Created');
+        });
+
+    } else if (req.method === 'DELETE') {
+        try {
+            await fs.promises.unlink(filePath);
+            res.statusCode = 200;
+            res.end('Deleted');
+        } catch {
+            res.statusCode = 404;
+            res.end('Not found');
+        }
+
+    } else {
+        res.statusCode = 405;
+        res.end('Method not allowed');
+    }
 });
 
 server.listen(parseInt(port), host, () => {
-  console.log(`Server running at http://${host}:${port}`);
+    console.log(`Server running at http://${host}:${port}`);
 });
